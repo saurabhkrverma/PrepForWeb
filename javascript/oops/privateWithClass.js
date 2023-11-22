082af0cfd066103fa21ce24281cd8b51438dbb83
sauraverma <sauraverma@paypal.com>
2023-11-22 17:41:34 +0530

    adding few new questions

---
+++ javascript/oops/privateWithClass.js
@@ -0,0 +1,33 @@
+
+class Person {
+    #privateName
+    constructor (name) {
+        this.#privateName = name
+    }
+
+    sayHello () {
+        console.log(`Hello this is ${this.#privateName}`)
+    }
+}
+
+
+class AnotherPerson {
+
+    constructor(name) {
+        let _name = name;
+        this.getName = function(){
+            return _name;
+        }
+    }
+
+    sayHello  = () => {
+        console.log(`Hello this is ${this?.getName()}`)
+    }
+}
+
+
+const Foo = new AnotherPerson("Sanju");
+const Woo = new AnotherPerson("Billu");
+
+Foo.sayHello();
+Woo.sayHello();
